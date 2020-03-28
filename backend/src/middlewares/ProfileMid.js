5c52695f1636c197389a9525f06420c5219bb5d4
João Henrique <joaohenrique.hs@hotmail.com>
2020-03-28 16:20:11 -0300

    Refactored middlewares in routes.js file

---
+++ backend/src/middlewares/ProfileMid.js
@@ -0,0 +1,12 @@
+const { celebrate, Segments, Joi } = require('celebrate')
+
+module.exports = {
+    index() {
+        const middleware = celebrate({
+            [Segments.HEADERS]: Joi.object({
+                authorization: Joi.string().required(),  
+             }).unknown(),
+        })
+        return (middleware)
+    }
+}
